4e022b2d6bedb3513dbf4b4bf75c38569c015062
joaorodrigues3099 <joaorodrigues3099@gmail.com>
2024-08-30 13:44:13 +0200

    Norminette[100%], Mini[100%]

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-alm <joao-alm@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/06 07:59:28 by joao-alm          #+#    #+#             */
+/*   Updated: 2024/08/06 17:49:59 by joao-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar('0' + (a / 10));
+			ft_putchar('0' + (a % 10));
+			ft_putchar(' ');
+			ft_putchar('0' + (b / 10));
+			ft_putchar('0' + (b % 10));
+			if (a != 98 || b != 99)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb2();
+	write(1, "\n", 1);
+	return (0);
+}
+*/
